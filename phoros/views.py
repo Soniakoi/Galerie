5f6700dd4e223e86981a63619a8df02703d87091
Soniawangui <koisonia99@gmail.com>
2019-07-21 21:49:14 +0300

    Create a view function that will be responsible to show all the images

--- phoros/views.py
+++ phoros/views.py
@@ -4,3 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome!')
+
+def gallery(request):
+  all_images = Image.objects.all()
+  return render(request,'make-gallery/gallery.html',{'all_images': all_images})
+
