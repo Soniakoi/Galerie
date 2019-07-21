60cd5f0f30bfdfd9223596a3d474acf5a0d80b4b
Soniawangui <koisonia99@gmail.com>
2019-07-21 23:51:18 +0300

    Create a migration file

--- phoros/models.py
+++ phoros/models.py
@@ -5,5 +5,5 @@ class Image(models.Model):
     image = models.ImageField(max_length =30)
     name = models.CharField(max_length =30)
     description = models.TextField()
-    location = models.Foreignkey(Location)
-    category = models.ForeignKey(Category)
+    # location = models.Foreignkey(Location)
+    # category = models.ForeignKey(Category)
