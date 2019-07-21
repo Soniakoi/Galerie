1a05cd59c55e7046b061fa8ae525f10b994672cc
Soniawangui <koisonia99@gmail.com>
2019-07-21 23:47:06 +0300

    Create a class image

--- phoros/models.py
+++ phoros/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(max_length =30)
+    name = models.CharField(max_length =30)
+    description = models.TextField()
+    location = models.Foreignkey(Location)
+    category = models.ForeignKey(Category)
