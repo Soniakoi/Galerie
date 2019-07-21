69c510c5155ca908dd8c2e417296f2906ac4f294
Soniawangui <koisonia99@gmail.com>
2019-07-21 23:29:18 +0300

    Create database and change content of the DATABASES settings and replace the user and password with my psql username and password

--- gallery/settings.py
+++ gallery/settings.py
@@ -79,6 +79,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'galerie',
+        'USER': 'sonia',
+    'PASSWORD':'Soniawangui99',
     }
 }
 
