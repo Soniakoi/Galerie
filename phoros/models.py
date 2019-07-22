44b59d7ab3b4744c7d240ed4c867bf7137cd6a87
Soniawangui <koisonia99@gmail.com>
2019-07-22 08:13:03 +0300

    Change the character field from 30 to 40

--- phoros/models.py
+++ phoros/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Image(models.Model):
-    image = models.ImageField(max_length =30)
-    name = models.CharField(max_length =30)
+    image = models.ImageField(max_length =40)
+    name = models.CharField(max_length =40)
     description = models.TextField()
     # location = models.Foreignkey(Location)
     # category = models.ForeignKey(Category)
 
 
 class Location(models.Model):
-  name = models.CharField(max_length = 30)    
+  name = models.CharField(max_length = 40)    
 
 
   def __str__(self):
@@ -21,8 +21,9 @@ class Category(models.Model):
   name = models.CharField(max_length=40)
 
 
-    @classmethod
-    def search_by_category(cls,search_term):
+
+  @classmethod
+  def search_by_category(cls,search_term):
         image_result = cls.objects.filter(category__name__icontains=search_term)
         return image_result
 
--- phoros/urls.py
+++ phoros/urls.py
@@ -5,7 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.welcome,name = 'galerie'),
-    url(r'^search/', views.search_images, name='search_images'),
+    url(r'^image/(\d+)',views.image,name='image'),
+   url(r'^search/',views.search_images,name='search_images'),
+   url(r'^gallery/',views.gallery,name='gallery'),
+   url(r'^location/(\w+)',views.image_location,name='location')
 ]
 
 if settings.DEBUG:
