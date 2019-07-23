e57114b4a5dadb7bf6ba51d355985b63121621be
Soniawangui <koisonia99@gmail.com>
2019-07-23 12:57:04 +0300

    Deploy

--- phoros/models.py
+++ phoros/models.py
@@ -19,7 +19,7 @@ class Location(models.Model):
 
 
 class Category(models.Model):
-  name = models.CharField(max_length=40)
+  name = models.CharField(max_length=70)
 
   def __str__(self):
     return self.name    
