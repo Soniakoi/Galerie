1ddc66955c1f13ec46cf4f72a51aa925ae86b9f4
Soniawangui <koisonia99@gmail.com>
2019-07-25 11:22:04 +0300

    modify tests

--- phoros/tests.py
+++ phoros/tests.py
@@ -1,42 +1,168 @@
 from django.test import TestCase
-from .models import Image, Location,Category
+from .models import Image, Location, Category
 
 # Create your tests here.
-class GalleryTestClass(TestCase):
-  #Set up method and saving instances
-  def setUp(self):
-    #location
-    self.mombasa = Location(name='Mombasa')
-    self.mombasa.save()
-    #category
-    self.sports = Category(name='Sports')
-    self.sports.save()
-    #image
-    self.kids_weekend = Image(image ='sneakers.jpeg',name='Sneakers',description='Bright colored and light',category=self.sports,location=self.mombasa)
-    self.kids_weekend.save()
-    
+class LocationTestClass(TestCase):
+    '''
+    Defines test cases for the Location class behaviours.
+      
+    Args:
+        TestCase: A class that helps create the test cases.
+    '''
+    def setUp(self):
+        '''
+        Method that runs before the test cases.
+        '''
+        self.location = Location()
 
+    def test_instance(self):
+        '''
+        Test case to see if the object is an instance of the Location class.
+        '''
+        self.assertTrue(isinstance(self.location,Location))
 
-  #Testing instance
-  def test_instance(self):
-    self.assertTrue(isinstance(self.kids_weekend,Image))
-    self.assertTrue(isinstance(self.mombasa,Location))
-    self.assertTrue(isinstance(self.sports,Category))
+    def test_save_method(self):
+        '''
+        Test case to see if the object is stored in the database.
+        '''
+        self.location.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
 
-  #Destroying the instance after test
-  def tearDown(self):
-    Image.object.all().delete()
-    Location.object.all().delete()
-    Category.object.all().delete()
+    def test_update_method(self):
+        '''
+        Test case to see if the object can be retrieved from the database and updated.
+        '''
+        self.location.save_location()
+        self.location = Location.objects.filter().update()
+        self.updated_location = Location.objects.get()
+        self.assertEqual(self.updated_location.name)
 
-  def delete_item(self):
-    self.mombasa.save()
-    test_location = Location('mombasa')
-    test_location.save_location
-    self.mombasa.delete()
-    self.assertEqual(len(Location.test_location),1)
+    def test_delete_method(self):
+        '''
+        Test case to see if the object can be deleted from the database.
+        '''
+        self.location.save_location()
+        self.location = Location.objects.get(id = 1)
+        self.location.delete_location()
+        self.assertTrue(len(Location.objects.all()) == 0)
 
-  def test_search_by_category(self):
-    self.kids_weekend.save()
-    instance= Category.objects.get(name=='sports')
-    self.assertTrue(instance.name=='sports')
+class ImageTestClass(TestCase):
+    '''
+    Defines test cases for the Image class behaviours.
+      
+    Args:
+        TestCase: A class that helps create the test cases.
+    '''
+    def setUp(self):
+        '''
+        Method that runs before the test cases.
+        '''
+        self.location = Location(name = "Nairobi")
+        self.location.save_location()
+        self.category = Category(name = "Test")
+        self.category.save_category()
+        self.image = Image(id = 1,name = "test", description = "test description", image_path = "path/to/image", category = self.category, location = self.location)
+
+    def test_instance(self):
+        '''
+        Test case to see if the object is an instance of the Image class.
+        '''
+        self.assertTrue(isinstance(self.image,Image))
+
+    def test_save_image(self):
+        '''
+        Test case to see if the object is stored in the database.
+        '''
+        self.image.save_image()
+        self.images = Image.objects.all()
+        self.assertTrue(len(self.images) > 0)
+
+    def test_get_image_by_id(self):
+        '''
+        Test case to see if an specific image can be retrieved from the database.
+        '''
+        self.image.save_image()
+        self.image = Image.get_image_by_id(1)
+        self.assertTrue(isinstance(self.image,Image))
+
+    def test_update_image(self):
+        '''
+        Test case to see if the object can be retrieved from the database and updated.
+        '''
+        self.image.save_image()
+        self.image = Image.objects.filter(id = 1).update(image_path = "new/path/to/image")
+        self.updated_image = Image.get_image_by_id(1)
+        self.assertEqual(self.updated_image.image_path,"new/path/to/image")
+
+    def test_search_by_category(self):
+        '''
+        Test case to see if images can be queried from the database based on the category.
+        '''
+        self.image.save_image()
+        self.category = Category(name = "test")
+        self.category.save_category()
+        self.searched_images = Image.search_by_category('test')
+        self.assertTrue(len(self.searched_images) > 0)
+
+    def test_filter_by_location(self):
+        '''
+        Test case to see if images can be queried from the database based on the location.
+        '''
+        self.image.save_image()
+        self.searched_images = Image.filter_by_location('')
+        self.assertTrue(len(self.searched_images) > 0)
+
+    def test_delete_image(self):
+        '''
+        Test case to see if the object can be deleted from the database.
+        '''
+        self.image.save_image()
+        self.searched_image = Image.get_image_by_id(1)
+        self.searched_image.delete_image()
+        self.assertTrue(len(Image.objects.all()) == 0)
+
+class CategoryTestClass(TestCase):
+    '''
+    Defines test cases for the Category class behaviours.
+      
+    Args:
+        TestCase: A class that helps create the test cases.
+    '''
+    def setUp(self):
+        '''
+        Method that runs before the test cases.
+        '''
+        self.category = Category(id = 1,name = 'test')
+
+    def test_instance(self):
+        '''
+        Test case to see if the object is an instance of the Category class.
+        '''
+        self.assertTrue(isinstance(self.category,Category))
+
+    def test_save_method(self):
+        '''
+        Test case to see if the object is stored in the database.
+        '''
+        self.category.save_category()
+        self.Category = Category.objects.all()
+        self.assertTrue(len(self.Category) > 0)
+
+    def test_update_method(self):
+        '''
+        Test case to see if the object can be retrieved from the database and updated.
+        '''
+        self.category.save_category()
+        self.category = Category.objects.filter(name = 'test').update(name = 'changed')
+        self.updated_category = Category.objects.get(name = 'changed')
+        self.assertEqual(self.updated_category.name,"changed")
+
+    def test_delete_method(self):
+        '''
+        Test case to see if the object can be deleted from the database.
+        '''
+        self.category.save_category()
+        self.category = Category.objects.get(id = 1)
+        self.category.delete_category()
+        self.assertTrue(len(Category.objects.all()) == 0)
